ec43877d0aa0d3eb1ebd140d108b4ba3056f7d54
Volynchuk <volynchuk18@gmail.com>
2019-05-15 21:14:41 +0300

    Finish logic part

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
-    <input @input="request" v-model="input">
-    <div v-if="response">{{response}}</div>
+    <input
+      @input="mapboxPlacesRequestHandler"
+      v-model="coordinationsInput"
+    >
+    <div v-if="mapboxPlacesResponse">
+      {{getTextArrayFromResponse}}
+    </div>
+    <input
+      v-model="locationInput"
+    >
+    <div>{{similarDataEnoughtLength}}</div>
   </div>
 </template>
 
@@ -10,26 +19,48 @@ import axios from 'axios';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      mapboxPlacesResponse: null,
+      coordinationsInput: null,
+      locationInput: null,
+    }
+  },
   methods: {
-    request() {
+    mapboxPlacesRequestHandler() {
+      if (!this.coordinationsInput) {
+        return
+      }
       const data = {
         type: 'address',
         access_token: 'pk.eyJ1IjoiYWtpdm1vb3ZleCIsImEiOiJjanVndGYzaW8wbnc3NDl0OWQ5dGhnMXFmIn0.FmA74joXKHPcNrPFb4B6nA'
       };
       try {
-        axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.input}.json`, {params: {...data}})
-            .then( res => {
-                this.response = res.data.features
-            })
+        axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.coordinationsInput}.json`, {params: {...data}})
+          .then( res => {
+            this.mapboxPlacesResponse = res.data.features
+          })
       } catch (e) {
-          alert(e)
+        console.error(e)
       }
-    }
+    },
   },
-  data() {
-    return {
-      response: null,
-      input: null,
+  computed: {
+    splitedLocationInput() {
+      return this.locationInput && this.locationInput.split(',').map(item => item.trim())
+    },
+    getTextArrayFromResponse() {
+      return this.mapboxPlacesResponse && this.mapboxPlacesResponse.map(item => item.text)
+    },
+    compareInputWithResponse() {
+      if (!this.getTextArrayFromResponse || !this.splitedLocationInput) {
+        return
+      } else {
+        return this.getTextArrayFromResponse.filter(value => this.splitedLocationInput.includes(value))
+      }
+    },
+    similarDataEnoughtLength() {
+      return this.compareInputWithResponse && this.compareInputWithResponse.length > (this.getTextArrayFromResponse.length / 2)
     }
   },
 }
@@ -37,9 +68,11 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  margin: 0 auto;
+  justify-content: center;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
