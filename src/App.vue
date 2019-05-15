c3600d951db7ed71fa90099a13bf289a7b454ff8
volynchuk <volynchuk18@gmail.com>
2019-05-15 18:25:05 +0300

    Complete axios request

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input @input="request" v-model="input">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  methods: {
+    request() {
+      const data = {
+        type: 'address',
+        access_token: 'pk.eyJ1IjoiYWtpdm1vb3ZleCIsImEiOiJjanVndGYzaW8wbnc3NDl0OWQ5dGhnMXFmIn0.FmA74joXKHPcNrPFb4B6nA'
+      };
+      axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${this.input}.json`, {params: {...data}})
+    }
+  },
+  data() {
+    return {
+      input: null,
+    }
+  },
 }
 </script>
 
