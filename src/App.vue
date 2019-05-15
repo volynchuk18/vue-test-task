4c5f0969684d98129f5c3365b27e69f365f23cd3
Volynchuk <volynchuk18@gmail.com>
2019-05-15 21:31:47 +0300

    Finish css part

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <input
+      class="location-input"
       @input="mapboxPlacesRequestHandler"
       v-model="coordinationsInput"
     >
-    <div v-if="mapboxPlacesResponse">
-      {{getTextArrayFromResponse}}
-    </div>
     <input
+      class="location-input"
       v-model="locationInput"
     >
-    <div>{{similarDataEnoughtLength}}</div>
+    <div
+      class="location__correct"
+      :style="{background: similarDataEnoughtLength ? 'green' : 'red'}"
+      v-html="validText"
+    >
+    </div>
   </div>
 </template>
 
@@ -46,35 +50,59 @@ export default {
     },
   },
   computed: {
+    validText() {
+      return this.similarDataEnoughtLength ? 'Valid' : 'Not valid';
+    },
     splitedLocationInput() {
-      return this.locationInput && this.locationInput.split(',').map(item => item.trim())
+      return this.locationInput && this.locationInput.split(',').map(item => item.trim());
     },
     getTextArrayFromResponse() {
-      return this.mapboxPlacesResponse && this.mapboxPlacesResponse.map(item => item.text)
+      return this.mapboxPlacesResponse && this.mapboxPlacesResponse.map(item => item.text);
     },
     compareInputWithResponse() {
       if (!this.getTextArrayFromResponse || !this.splitedLocationInput) {
         return
       } else {
-        return this.getTextArrayFromResponse.filter(value => this.splitedLocationInput.includes(value))
+        return this.getTextArrayFromResponse.filter(value => this.splitedLocationInput.includes(value));
       }
     },
     similarDataEnoughtLength() {
-      return this.compareInputWithResponse && this.compareInputWithResponse.length > (this.getTextArrayFromResponse.length / 2)
+      return this.compareInputWithResponse && this.compareInputWithResponse.length > (this.getTextArrayFromResponse.length / 2);
     }
   },
 }
 </script>
 
-<style>
-#app {
-  display: flex;
-  flex-direction: column;
-  width: 30%;
-  margin: 0 auto;
-  justify-content: center;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .location__correct {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .location-input {
+    margin-top: 15px;
+    height: 30px;
+    font-size: 22px;
+    border-radius: 6px;
+  }
+
+  .location__correct {
+    height: 100px;
+    border-radius: 50%;
+    width: 150px;
+    margin: 0 auto;
+    margin-top: 50px;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    width: 30%;
+    margin: 0 auto;
+    justify-content: center;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
